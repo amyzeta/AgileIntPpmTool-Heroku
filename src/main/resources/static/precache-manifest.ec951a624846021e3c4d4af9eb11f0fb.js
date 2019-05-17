b6223f4a99dffdf6b9bf2eace6a33ba32008f9e9
amy.zeta.wilson@gmail.com <amy.zeta.wilson@gmail.com>
2019-05-18 09:47:23 +1200

    resources for react app

---
+++ src/main/resources/static/precache-manifest.ec951a624846021e3c4d4af9eb11f0fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f9f0a1bfab6decf09dec",
+    "url": "/static/js/main.f9f0a1bf.chunk.js"
+  },
+  {
+    "revision": "9b97b6ce168dfc6e0b6b",
+    "url": "/static/js/1.9b97b6ce.chunk.js"
+  },
+  {
+    "revision": "f9f0a1bfab6decf09dec",
+    "url": "/static/css/main.eaf61071.chunk.css"
+  },
+  {
+    "revision": "9b97b6ce168dfc6e0b6b",
+    "url": "/static/css/1.8a2ca5d3.chunk.css"
+  },
+  {
+    "revision": "c97964acabd6181b8d7c0b4cdb7cb560",
+    "url": "/index.html"
+  }
+];
